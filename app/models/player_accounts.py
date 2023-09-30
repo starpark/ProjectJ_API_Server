b2f1a4c9c6707a08a3cd2133ca9b3c20db3fab56
starpark <62285975+starpark@users.noreply.github.com>
2023-09-30 20:20:49 +0900

    fix response status code

--- app/models/player_accounts.py
+++ app/models/player_accounts.py
@@ -1,7 +1,8 @@
 import datetime
 from sqlalchemy import Column, NVARCHAR, DATETIME, INT, BINARY
 from sqlalchemy.orm import validates
-
+from fastapi import HTTPException
+from starlette import status
 from app.database import Base
 
 class PlayerAccounts(Base):
@@ -9,31 +10,34 @@ class PlayerAccounts(Base):
 
     player_id = Column(INT, primary_key=True, nullable=False, autoincrement=True)
     name = Column("name", NVARCHAR(20), nullable=False)
-    nickname = Column("nickname",NVARCHAR(12), nullable=False)
-    password = Column("password",BINARY(60), nullable=False)
+    nickname = Column("nickname", NVARCHAR(12), nullable=False)
+    password = Column("password", BINARY(60), nullable=False)
     registration_date = Column(DATETIME, nullable=False, default=datetime.datetime.now)
 
     @validates("name")
     def validate_name(self, key, name) -> str:
-        if len(name) <= 2:
-            raise ValueError("name too short")
+        if len(name) < 2:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="아이디가 너무 짧습니다."
+            )
         elif len(name) > 20:
-            raise ValueError("name too long")
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="아이디가 너무 깁니다."
+            )
         return name
 
     @validates("nickname")
     def validate_nickname(self, key, nickname) -> str:
-        if len(nickname) <= 2:
-            raise ValueError("nickname too short")
+        if len(nickname) < 2:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="닉네임이 너무 짧습니다."
+            )
         elif len(nickname) > 12:
-            raise ValueError("nickname too long")
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="닉네임이 너무 깁니다."
+            )
         return nickname
-
-    @validates("password")
-    def validate_password(self, key, password) -> str:
-        if len(password) <= 6:
-            raise ValueError("password too short")
-        elif len(password) > 60:
-            raise ValueError("password too long")
-        return password
-
