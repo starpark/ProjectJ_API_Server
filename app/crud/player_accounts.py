59b18cfaac087825ff28ebe763a816aeba2a1a33
starpark <62285975+starpark@users.noreply.github.com>
2023-09-30 20:20:18 +0900

    feat name, nickname, password condition

--- app/crud/player_accounts.py
+++ app/crud/player_accounts.py
@@ -1,6 +1,7 @@
 import bcrypt
 from sqlalchemy.orm import Session
-
+from fastapi import HTTPException
+from starlette import status
 from app.models.player_accounts import PlayerAccounts
 from app.schemas.player_accounts import (
     PlayerAccountWithToken,
@@ -16,6 +17,12 @@ def get_player_by_nickname(db: Session, player_nickname: str):
     return db.query(PlayerAccounts).filter(PlayerAccounts.nickname == player_nickname).first()
 
 def create_player_account(db: Session, player_create: PlayerAccountCreate):
+    if len(player_create.password) < 6:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="비밀번호가 너무 짧습니다."
+        )
+
     hashed_password = bcrypt.hashpw(player_create.password.encode("utf-8"), bcrypt.gensalt())
     db_player_account = PlayerAccounts(name=player_create.name, nickname=player_create.nickname, password=hashed_password)
     db.add(db_player_account)
@@ -24,7 +31,9 @@ def create_player_account(db: Session, player_create: PlayerAccountCreate):
     return db_player_account
 
 def get_existing_player(db: Session, player_create: PlayerAccountCreate):
-    return db.query(PlayerAccounts).filter(
-        (PlayerAccounts.name == player_create.name) |
-        (PlayerAccounts.nickname == player_create.nickname)
-    ).first()
+    result = db.query(PlayerAccounts)
+    if result.filter(PlayerAccounts.name == player_create.name).first():
+        return "이미 존재하는 아이디입니다."
+
+    if result.filter(PlayerAccounts.nickname == player_create.nickname).first():
+        return "이미 존재하는 닉네임입니다."
